b1068814a183d4a9b18b1033c3a2e6f8221771ce
ececilla <ececilla@server>
2014-06-05 00:30:42 +0200

    first commit

--- README.md
+++ README.md
@@ -1,26 +1,53 @@
-# tec [![Build Status](https://secure.travis-ci.org/enric/tec.png?branch=master)](http://travis-ci.org/enric/tec)
+[![Build Status](https://secure.travis-ci.org/ececilla/tec.png?branch=master)](http://travis-ci.org/ececilla/tec)
+[![NPM version](https://badge.fury.io/js/tec.svg)](http://badge.fury.io/js/tec)
+#tec: Time Eviction Cache#
 
-Time eviction cache
+_tec_ module is a time eviction cache with an integration via proxy injection that makes its work transparent. The usage is very simple, inject the cache into a target function and let _tec_ keep track of the returned values from the target function for you. 
 
-## Getting Started
-Install the module with: `npm install tec`
+No more checking for cache hits and misses that pollutes your business logic. _tec_ handles it under the hood and retrieves the information again when internal values have been evicted. It works for both synchronous and asynchronous target functions.
+
+## Example
 
 ```javascript
 var tec = require('tec');
-tec.awesome(); // "awesome"
+var mymodule = {
+	sum:function(x,y){
+		console.log("sum(" + x + "," + y + ")");
+		return x+y;
+	}
+}
+tec.cache(mymodule,"sum",1000);//1 second eviction
+
+mymodule.sum(5,5);
+mymodule.sum(5,5);
+
+```
+The first time that the _sum_ function is called _tec_ routes the call to the target function, holds the value returned in memory so the next calls are served from within _tec_ itself. After the timeout the value is evicted, so a future call will be rerouted again to the source. This feature can be very handy for asynchronous fetches to a database or remote system to lower down the number of requests.
+
+Following this line you can find the an example with an asynchronous target function.
+
+```javascript
+var tec = require('tec');
+var mymodule = {
+	sum:function(x,y){
+		setTimeout(function(){
+			cb(null, x+y);
+		},5000);
+	}
+}
+tec.cache(mymodule,"sum",1000);//1 second eviction
+
+mymodule.sum(5,5,function(err,val){
+	//val served from source
+	mymodule.sum(5,5,function(err,val){
+        //val served from internal cache
+	});	
+});
+
 ```
 
-## Documentation
-_(Coming soon)_
-
-## Examples
-_(Coming soon)_
-
-## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
-
-## Release History
-_(Nothing yet)_
+## To run the tests:
+    $ grunt nodeunit
 
 ## License
 Copyright (c) 2014 Enric Cecilla
--- test/tec_test.js
+++ test/tec_test.js
@@ -1,47 +1,69 @@
 'use strict';
-
 var tec = require('../lib/tec.js');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
-exports['awesome'] = {
+exports['tec'] = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'set_proxy_cache': function(test) {
-    var x = 0;
+  tearDown:function(done){
+
+    tec.clear();
+    done();
+  },
+  "cache 1 module": function(test) {
+    var count = 0;
     var mymodule = {
       sum:function(x,y){
-        x = x+1;
+        count = count+1;
         return x+y;
       }
-    }
-    tec.set_proxy_cache("sum");
-    tec.cache(mymodule,1000);
+    }    
+    tec.cache(mymodule,"sum",500);
     var result = mymodule.sum(2,2);
     test.equal(4,result);
-    result = mymodule.sum()
-    // tests here
-    test.equal(tec.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+    test.equal(1,count);
+    result = mymodule.sum(2,2);
+    test.equal(4,result);
+    test.equal(1,count);
+    setTimeout(function(){
+      result = mymodule.sum(2,2)
+      test.equal(4,result);
+      test.equal(2,count);
+      test.expect(6);
+      test.done();
+    },1000);
+        
   },
+  "cache 2 modules": function(test) {
+    var count1 = 0,
+        count2 = 0,
+        mymodule1 = {
+          sum:function(x,y){
+            console.log("sum...")
+            count1 = count1+1;
+            return x+y;
+          }
+        },
+        mymodule2 = {
+          mult:function(x,y){
+            console.log("mult..")
+            count2 = count2+1;
+            return x*y;
+          }
+        };    
+    tec.cache(mymodule1,"sum",500);
+    tec.cache(mymodule2,"mult",500);
+    var result = mymodule1.sum(2,2);
+    test.equal(4,result);
+    test.equal(1,count1);
+    test.equal(0,count2);
+    result = mymodule2.mult(2,2);
+    test.equal(4,result);
+    test.equal(1,count1);
+    test.equal(1,count2);
+    test.expect(6);
+    test.done();
+    
+  }
 };
